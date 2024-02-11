d3ac17c9e9443ac4b98031c29d4c0554a9c72f05
Adedayo Okubanjo <dayookubanjo@yahoo.com>
2024-02-11 01:13:54 -0500

    GitHub Actions Setup File

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Food
 
 # Create your tests here.
+class ModelTests(TestCase):
+
+    def setUp(self):
+        self.new_food = Food.objects.create(
+            name = 'Potato',
+            description = 'Irish Potato',
+            calories = 135.68,
+            cabohydrate = 32.5,
+            protein = 0.96,
+            fat = 2.54,
+            measurement_scale = 'grams',
+            serving_size = '100 grams'
+            )
+        
+    def test_food_model(self):
+        data = self.new_food
+        #Check that new_food is an instance of Food
+        self.assertTrue(isinstance(data, Food))
+        self.assertEqual(str(data), 'Potato-->135.68')
